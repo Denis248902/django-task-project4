18711646c19cc8365252c4f08c28b60f9547ad18
Денис Постовойтов <denisdotniy@yandex.ru>
2026-08-01 17:22:10 +0300

    feat: add JWT token endpoints to collects URLs

--- collects/urls.py
+++ collects/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from .viewsets import CollectViewSet, PaymentViewSet
+from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'collects', CollectViewSet, basename='collect')
@@ -8,4 +12,6 @@ router.register(r'payments', PaymentViewSet, basename='payment')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
