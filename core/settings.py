ffa58675a38e599ae4cfda70cb79a1ca73b4f382
Денис Постовойтов <denisdotniy@yandex.ru>
2026-08-01 17:29:12 +0300

    feat: configure global DRF pagination (PageNumberPagination, page_size=10)

--- core/settings.py
+++ core/settings.py
@@ -148,3 +148,14 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
