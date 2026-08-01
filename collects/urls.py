e16dddbeea62fb1413dafde446dbc81c8459a7dc
Денис Постовойтов <denisdotniy@yandex.ru>
2026-08-01 17:40:13 +0300

    feat: register collects router in urls.py

--- collects/urls.py
+++ collects/urls.py
@@ -1,17 +1,11 @@
-from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from .viewsets import CollectViewSet, PaymentViewSet
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from .viewsets import CollectViewSet
+
 router = DefaultRouter()
-router.register(r'collects', CollectViewSet, basename='collect')
-router.register(r'payments', PaymentViewSet, basename='payment')
+router.register(r"collects", CollectViewSet, basename="collect")
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("", include(router.urls)),
 ]
