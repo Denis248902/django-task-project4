5869ee21d662ef990d0ac016a2ae80e7610dd703
Денис Постовойтов <denisdotniy@yandex.ru>
2026-08-01 19:07:13 +0300

    fix: remove debug prints, keep correct author assignment in CollectViewSet

--- collects/viewsets.py
+++ collects/viewsets.py
@@ -1,19 +1,13 @@
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters, viewsets
-
-from .models import Collect
-from .serializers import CollectSerializer
+from rest_framework import viewsets
+from collects.models import Collect
+from collects.serializers import CollectSerializer
+from api.permissions import IsEditorOrReadOnly
 
 
 class CollectViewSet(viewsets.ModelViewSet):
     queryset = Collect.objects.all()
     serializer_class = CollectSerializer
-    filter_backends = [
-        DjangoFilterBackend,
-        filters.OrderingFilter,
-        filters.SearchFilter,
-    ]
-    filterset_fields = ["reason"]
-    search_fields = ["title", "description"]
-    ordering_fields = ["deadline", "created_at", "target_amount"]
-    lookup_field = "id"
+    permission_classes = [IsEditorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
